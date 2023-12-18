3cd1dfc9ea1308fd42c04a71d485ba9b272eb923
agustinallamanocosta <allamanocostaagustin@gmail.com>
2023-12-19 03:24:06 +1100

    feat: add base version of the node with helathcheck working

--- src/api/service.py
+++ src/api/service.py
@@ -1,35 +1,47 @@
 from zkp.api.zkp import random_private_input
-import subprocess
+from subprocess import call
 
 DEFAULT_CHAIN_LENGTH: int = 3
 
 def run_rescue_prover():
 
     executable = "/src/zkp/build/Release/src/starkware/main/rescue/rescue_prover"
-    cmd = "--parameter_file src/zkp/examples/rescue/rescue_params.json \
-        --prover_config_file src/zkp/examples/rescue/rescue_prover_config.json \
-        --public_input_file src/zkp/examples/rescue/rescue_public_input.json \
-        --private_input_file src/zkp/examples/rescue/rescue_private_input.json \
-        --out_file src/zkp/examples/rescue/proof.json \
-        --logtostderr"
-    run_process(executable,cmd)
+    cmd = ["--parameter_file",
+           "/src/zkp/examples/rescue/rescue_params.json",
+           "--prover_config_file",
+           "/src/zkp/examples/rescue/rescue_prover_config.json",
+           "--public_input_file",
+           "/src/zkp/examples/rescue/rescue_public_input.json",
+           "--private_input_file",
+           "/src/zkp/examples/rescue/rescue_private_input.json",
+           "--out_file",
+           "/src/zkp/examples/rescue/proof.json",
+           "--logtostderr"
+    ]
+    proc = call([executable] + cmd)
+    return proc
 
 def run_rescue_validation():
 
-    executable = "/src/zkp/build/Release/src/starkware/main/rescue/rescue_verifie"
-    cmd = "--in_file src/zkp/examples/rescue/proof.json \
-        --logtostderr"
-    run_process(executable,cmd)
-
-def run_process(executable,cmd):
-
-    proc = subprocess.Popen(executable=executable,args=cmd,shell=True)
-    outs, errs = proc.communicate(timeout=15)
-    print("out puts",outs)
-    print("errors ",errs)
+    executable = "/src/zkp/build/Release/src/starkware/main/rescue/rescue_verifier"
+    cmd = [
+        "--in_file",
+        "/src/zkp/examples/rescue/proof.json",
+        "--logtostderr"
+    ]
+    proc = call([executable] + cmd)
+    return proc
 
 def run_helath_check_test():
+    try:
+        random_private_input(chain_length=DEFAULT_CHAIN_LENGTH)
+        prover_response = run_rescue_prover()
+        validation_response = run_rescue_validation()
+        if prover_response > 0 or validation_response > 0:
+            return False
+        else:
+            return True
+    except Exception as e:
+        print(e)
+        return False
 
-    random_private_input(chain_length=DEFAULT_CHAIN_LENGTH)
-    run_rescue_prover()
-    run_rescue_validation()
--- src/zkp/api/zkp.py
+++ src/zkp/api/zkp.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import json
-import os
 from zkp.src.starkware.air.rescue.rescue_constants import PRIME
 from zkp.src.starkware.main.rescue.rescue_end_to_end_test import generate_random_hex_witness_and_output
 
@@ -9,10 +8,13 @@ def random_private_input(chain_length:int):
 
     witness_and_output = generate_random_hex_witness_and_output(PRIME, chain_length)
     witness, output = witness_and_output['witness'], witness_and_output['output']
-    example_dir = os.path.dirname(__file__)
-    json.dump({'witness': witness}, open(
-        os.path.join(example_dir, 'rescue_private_input.json'), 'w'), indent=4)
     json.dump(
-        {'output': output, 'chain_length': chain_length},
-        open(os.path.join(example_dir, 'rescue_public_input.json'), 'w'),
-        indent=4)
+        {'witness': witness}
+        ,open('/src/zkp/examples/rescue/rescue_private_input.json', 'w')
+        , indent=4
+    )
+    json.dump(
+        {'output': output, 'chain_length': chain_length}
+        ,open('/src/zkp/examples/rescue/rescue_public_input.json', 'w')
+        ,indent=4
+    )
